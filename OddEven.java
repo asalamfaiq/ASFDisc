f96e4f0341b7902cedfded4e5779ba4baf82d0dc
asalamfaiq <71775709+asalamfaiq@users.noreply.github.com>
2020-09-23 20:09:23 +0430

    2.1

--- OddEven.java
+++ OddEven.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 public class OddEven{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter a number, or if you want to stop the execution enter -1: ");
+        System.out.print("Please enter a number, or the execution can be stopped by entering -1: ");
         int num= input.nextInt();
         while(num!=-1){
             if(oddEven(num)==1)
                 System.out.println("The " + num + " is Even" );
             else
                 System.out.println("The " + num + " is Odd" );
-            System.out.print("Please enter a number, or if you want to stop the execution enter -1: ");
+            System.out.print("Please enter a number, or the execution can be stopped by entering -1: ");
             num= input.nextInt();
         }
     }
